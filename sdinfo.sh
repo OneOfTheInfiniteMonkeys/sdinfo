3a3d30a47ab765b4cc0daff2b349f9a7dcb3eabb
OneOfTheInfiniteMonkeys <45082871+OneOfTheInfiniteMonkeys@users.noreply.github.com>
2021-12-28 23:28:49 +0000

    Update

--- sdinfo.sh
+++ sdinfo.sh
@@ -60,8 +60,8 @@ readonly COL_Spinner=$(tput setaf 6)              # Cyan
 #---------------------------------------
 cmd_additional="n"                                # Don't output additional detection information
 cmd_minimal="n"                                   # Install all needed and optional pacakges for fully featured installation
-cmd_cidsrc=""                                     # Default path to start seaching for CID - reduces potential miss hits
-
+cmd_cidsrc="/"                                    # Default path to start seaching for CID - reduces potential miss hits
+cmd_extcid=""
 
 #---------------------------------------
 # Process Command Line Parameters
@@ -71,30 +71,44 @@ PROG=${0##*/}                                     # Get script name to reporting
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -a|--additional)
-            printf "      - Additional information will be output during running\n"
+            printf "      - Additional information will be output\n"
             cmd_additional="y"
             ;;
+        -c|--cid)
+		    tmp=$2
+		    if [[ ${#tmp} < 32 ]] ; then
+			  printf "Supplied CID too short\n"
+			  exit 1
+			fi
+		    if [[ ${#tmp} > 33 ]] ; then
+			  printf "Supplied CID too long\n"
+			  exit 1
+			fi
+			tmp=""
+		    cmd_extcid=$2
+			;;
         -m|--minimal)
-            printf "      - Minimal output infomation. i.e. No banners or other messages\n"
+            # Minimal output infomation. i.e. No banners or other messages
             cmd_minimal="y"
             ;;
         -s|--sourcepath)
-		    
-		    if [[ $([ -d ${OPTARG} ]) = 0 ]] ; then
-			  printf "Directory %s \n" "${OPTARG}"
-			fi
-			 
-            printf "      - Parameter specifying alternate path to search for CID file\n"
+ 		    if [ -d $2 ] ; then
+		      printf "Directory %s \n" "$2"
+			  cmd_cidsrc=$2  
+			else 
+			  printf "Unable to locate folder\n"
+              exit 1			
+		    fi
             ;;
         -v|--version)
-            printf "      %s %s %s ( %s %s ) \n" "$App_Name" "$PROG" "$installer_version" "$installer_date" "$installer_time"
+            printf "      %s %s %s ( %s %s ) \n" "$App_Name" "$PROG" "$App_version" "$App_rel_date" "$App_rel_time"
             exit 0
             ;;
         -h|--help|-?)
             printf " Usage: %s [OPTION...] [COMMAND]...\n" "$PROG"
             printf " Options:\n"
-            printf "   -a, --additional       Output additional information during installation\n"
-            printf "   -m, --minimal          Only minimal ouptut from the script is output.\n"
+            printf "   -a, --additional       Output additional information during operation\n"
+            printf "   -m, --minimal          Only minimal ouptut from the script.\n"
             printf "                            i.e. No banners or other messages.\n"
             printf " Commands:\n"
             printf "   -h, --help, -?         Displays this help and exits\n"
@@ -102,13 +116,24 @@ while [[ $# -gt 0 ]]; do
             printf " Examples:\n"
             printf "   %s -h \n" "$PROG"
             printf "   %s -a \n" "$PROG"
-            printf "\n"
+			printf "   %s -c 1b534d454332515430615763c7013633" "$PROG"
+            printf "\n\n"
+			printf " Sample Output                Decoded\n"
+			printf "    MID : Samsung           - Manufacturer name\n"
+			printf "    OID : SM                - OEM identifier\n"
+			printf "    PNM : EC2QT             - Part Name\n"
+			printf "    PRV : 30                - Part revision\n"
+			printf "    PSN : 1633117127        - Product serial number\n"
+			printf "    MDT : 2019/1/1          - Date yyyy/mm/dd\n"
+			printf "    AID : 1092              - Age in days from MDT\n"
+            printf "\n\n"
+			printf " All data for indication only\n"
             exit 0
             ;;
-         *)
-            printf "      - Unrecognised option\n"
-            exit 0
-            ;;
+        # *)
+        #    printf "      - Unrecognised option\n"
+        #    exit 0
+        #    ;;
     esac # End of case
     shift
 done # End of While command line options
@@ -126,37 +151,44 @@ function Logo() {
 #                  : None
 #------------------:
 # Globals          :
-#                  : $COL_Logo,  ${COL_WhiteText}, ${App_Name}, ${installer_date}
+#                  : $COL_Logo,  $COL_WhiteText, $App_Name, ${App_rel_date}
 #------------------:
 
-     printf "%s" "$COL_Logo"
-     printf "\n"
-     printf "       ___________________  \n"
-     printf "      |                   | \n"
-     printf "      | |               ==| \n"
-     printf "      | |               ==| \n"
-     printf "      | |  %s" "$COL_WhiteText"
-     printf "%-.15s" " $App_Name      "
-     printf "%s==| \n" "$COL_Logo"
-     printf "      | |   ${App_rel_date}  ==| \n"
-     printf "      | |               ==| \n"
-     printf "      | |       __    ____| \n"
-     printf "      |________/  |__/      \n"
-     printf "\n"
+  printf "%s" "$COL_Logo"
+  printf "\n"
+  printf "       ___________________  \n"
+  printf "      |                   | \n"
+  printf "      | |               ==| \n"
+  printf "      | |               ==| \n"
+  printf "      | |  %s" "$COL_WhiteText"
+  printf "%-.15s" " $App_Name      "
+  printf "%s==| \n" "$COL_Logo"
+  printf "      | |   ${App_rel_date}  ==| \n"
+  printf "      | |               ==| \n"
+  printf "      | |       __    ____| \n"
+  printf "      |________/  |__/      \n"
+  printf "\n"
 }
 
 
 #--------><--------><--------><-------->
 # Output Install Logo to the terminal
-Logo
+if [[ $cmd_minimal = "n" ]] ; then
+  Logo
 
-printf " - All data for indication only !"
+  printf " - All data for indication only !"
+  printf "\r                                  "
+  printf "\n"
 
-cidpath=$(find / -name 'cid' -print 2>/dev/null)
-cidinfo=$(cat $cidpath)
-printf "\r                                  "
-printf "\n"
+fi  
 
+if [[ $cmd_extcid = "" ]]; then 
+  cidpath=$(find $cmd_cidsrc -name 'cid' -print 2>/dev/null)
+  cidinfo=$(cat $cidpath)
+else
+  cidpath="/"
+  cidinfo=$cmd_extcid
+fi  
 
 if [[ $cidpath == "" ]]; then
   # Appears as if there is no SD
@@ -182,6 +214,7 @@ if [[ ${#cidinfo} < 32  ]] ; then
   exit 2
 fi
 
+# Decode CID
 # example               1b534d454332515430615763c7013633
 #                       1b 534d 4543325154 30 615763c7 0136 33
 mid=${cidinfo:0:2} #     2 chars - 1 byte   - Unique manufacturer identifier
@@ -191,9 +224,10 @@ prv=${cidinfo:16:2} #    2 chars - 1 bytes  - Manf. Version (BCD)
 psn=${cidinfo:18:8} #    8 chars - 4 bytes  - Manf. Serial No. 32 bit uint
 mdt=${cidinfo:27:3} #    3 chars - 1.5 byte - Manf. Date - 12 bits - BCD YYM (YY + 2000)
 crc=${cidinfo:30:2} #    2 chars - 1 byte   - Data CRC - 7 bits (LSB always 1 i.e. odd)
-((mdt&=4095))       #    Limit MDT to 12 bits
+#((mdt&=4095))       #    Limit MDT to 12 bits
 
 if [[ $cmd_additional = "y" ]]; then
+  # output raw infromation from CID
   printf "MID : %s\n" "$mid"
   printf "OID : %s\n" "$oid"
   printf "PNM : %s\n" "$pnm"
@@ -207,7 +241,7 @@ fi
 # The principle applied is source identified as manufacturer where possible,
 # which may not match marking see www.bunniestudios.com/blog/?page_id=1022
 # Sources - Manufacturers data sheets, some SD cards, 
-# cross checked with a number of sources in addition (in no specific order):
+# Cross checked with a number of sources in addition (in no specific order):
 # elinux.org/RPi_SD_cards github.com/kbiva/stm32f103_projects https://goughlui.com/ www.cameramemeoryspeed.com 
 d_mid='' #                                        Decoded primary brand
 d_mid_id='' #                                     2 character ID
@@ -220,6 +254,7 @@ case $mid in
   '02')
     d_mid='Toshiba'
 	d_mid_id='TM'
+	d_mid_aka='Kingston'
 	;;
   '03')
     d_mid='Sandisk'
@@ -273,42 +308,34 @@ case $mid in
   '30')
     d_mid='Sandisk'
     d_mid_id='SD'
-	d_mid_aka=''
 	;;
   '31')
     d_mid='Silicon Power'
     d_mid_id='SP'
-	d_mid_aka=''
 	;;
   '41')
     d_mid='Kingston'
     d_mid_id='SP'
-	d_mid_aka=''
 	;;
   '5d')
     d_mid='Swissbit'
     d_mid_id='SB'
-	d_mid_aka=''
 	;;
   '61')
     d_mid='Netlist'
     d_mid_id='NL'
-	d_mid_aka=''
 	;;
   '63')
     d_mid='Cactus'
     d_mid_id='CL'
-	d_mid_aka=''
 	;;
   '6f')
     d_mid='Plantinum'
     d_mid_id='PL'
-	d_mid_aka=''
 	;;
   '73')
     d_mid='Fuji'
     d_mid_id='FJ'
-	d_mid_aka=''
 	;;
   '74')
     d_mid='Jialec'
@@ -318,32 +345,28 @@ case $mid in
   '76')
     d_mid='Patriot'
     d_mid_id='PA'
-	d_mid_aka=''
 	;;
   '82')
-    d_mid='Jangtay'
+    d_mid='Sony'
     d_mid_id='JT'
-	d_mid_aka=''
+	d_mid_aka='Jangtay'
 	;;
   '83')
     d_mid='Netcom'
     d_mid_id='NC'
-	d_mid_aka=''
 	;;
   '84')
     d_mid='Strontium'
     d_mid_id='ST'
-	d_mid_aka=''
 	;;
   '88')
     d_mid='Pretec'
     d_mid_id='0x03 0x02'
-	d_mid_aka=''
 	;;
   '89')
     d_mid='MemoryStar'
     d_mid_id='JT'
-	d_mid_aka=''
+	d_mid_aka="Team"
 	;;
   '92')
     d_mid='Sony'
@@ -358,7 +381,6 @@ case $mid in
   '9f')
     d_mid='Blackweb'
     d_mid_id='BW'
-	d_mid_aka=''
 	;;
 	
   *)
@@ -370,9 +392,13 @@ esac
 
 # Decode the OEM ID a two character ASCII code frm the two hex OID bytes
 d_oid=""
-d_oid=$(printf '0x'${oid:0:2}'.0x'+${oid:2:2} | xxd -r)
+if [[ $((16#${oid:0:2})) < 32 ]] || [[ $((16#${pnm:2:2})) < 32 ]] ; then
+  d_oid=$((16#${oid:0:2}))$((16#${oid:2:2}))
+else
+  d_oid=$(printf '0x'${oid:0:2}'.0x'+${oid:2:2} | xxd -r)  
+fi  
 
-# Decode the Part Number from hex to decimal
+# Decode the Part Name from hex to decimal
 d_pnm=""
 d_pnm=$(printf '0x'${pnm:0:2}'.0x'${pnm:2:2}'.0x'${pnm:4:2}'.0x'${pnm:6:2}'.0x'${pnm:8:2} | xxd -r)
 
@@ -387,20 +413,38 @@ d_psn=$((16#$psn))
 d_mdt=""
 d_mdt=$((16#${mdt:0:2})) #                        Extract the year from the CID
 d_mdt=$((d_mdt + 2000))  #                        Add the centry offset of 2000
-d_mdm=$((16#${mdt:2:1})) #                   Extract the month 
+if [[ $d_mdt > $(date +%Y) ]] ; then  #           Probable date coding error - not to SD format rules
+  d_mdt=2000
+fi
+
+d_mdm=$((16#${mdt:2:1})) #                        Extract the month 
+if [[ $d_mdm > 12 ]] || [[ d_mdm = 0 ]]; then #   Probable date coding error - not to SD format rules
+  d_mdm=1
+fi 
+
 d_mdt=$d_mdt"/"$d_mdm #                           Concatenate Year and month with sperator
+# Calculate approximate number of days - assume 1st of month
+d_ndy=$(( ($(date +%s ) - $(date --date="$d_mdt"/1 +%s)) / (86400)  )) # 
 
 
 # Output the decoded data
 printf "MID : %s\n" "$d_mid"
-if [[ $d_mid_aka> '' ]]; then
-  printf "AKA : %s\n" "$d_mid_aka"
+if [[ $d_mid_aka > '' ]] ; then
+  if [[ $cmd_additional = "y" ]] ; then
+    printf "AKA : %s\n" "$d_mid_aka"
+  fi	 
 fi
 printf "OID : %s\n" "$d_oid"
 printf "PNM : %s\n" "$d_pnm"
 printf "PRV : %s\n" "$d_prv"
 printf "PSN : %s\n" "$d_psn"
-printf "MDT : %s\n" "$d_mdt"
+if [[ $cmd_minimal = "n" ]] ; then
+  printf "MDT : %s\n" "$d_mdt/1"
+else
+  printf "MDT : %s\n" "$d_mdt"
+fi  
+if [[ $cmd_minimal = "n" ]] ; then
+  printf "AID : %s\n" "$d_ndy"
+fi  
 printf "\n"
-
 
